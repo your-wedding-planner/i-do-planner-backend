9ebb605010082ee76b5ef43b25a8492f3c2edc11
“elisejonkers” <elisejonkers18@hotmail.com>
2023-12-07 10:56:00 +0100

    changed port to 5174

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(
     cors({
-    origin: ["http://localhost:5173", "http://localhost:5005"],
+    origin: ["http://localhost:5174", "http://localhost:5005"],
     })
     );
 
